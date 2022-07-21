fe6d0405b002aa79f12f2a279a3f6609a66ab882
mojvaf <mvafa44@gmail.com>
2022-07-21 09:34:19 +0200

    login

--- client/src/components/dashboard/Dashboard.tsx
+++ client/src/components/dashboard/Dashboard.tsx
@@ -5,7 +5,13 @@ import list from "../../util";
 
 const Dashboard: React.FC = () => {
   const [projects, setProjects] = useState(list());
-  const [currentProject, setCureentProject] = useState(projects[0]);
+  const [currentProject, setCurrentProject] = useState(projects[0]);
+
+  
+ 
+ const handelChangeProject=()=>{
+  
+ }
   return (
     <section>
       <div className="project-container">
@@ -17,7 +23,7 @@ const Dashboard: React.FC = () => {
       <div className="library">
         <h2>List of projects</h2>
         {projects.map((project) => (
-          <div className="library-list">
+          <div  className={`library-list ${project.active ? 'selected' : ''}`}>
             <img className="img" src={project.cover} alt={project.artist} />
             <div className="library-description">
               <h3>{project.name}</h3>
--- client/src/components/projects/Projects.tsx
+++ client/src/components/projects/Projects.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './project.css'
 
 const Projects:React.FC = () => {
   return (
