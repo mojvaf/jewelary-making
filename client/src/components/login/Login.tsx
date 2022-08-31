e08848d3e0549ba709e1d2b6301f957167c7060c
mojvaf <mvafa44@gmail.com>
2022-08-31 08:48:04 +0200

    add some css

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,9 +1,9 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./login.css";
-import { useAppDispatch} from "../../redux/store";
+import { Link } from "react-router-dom";
+import { useAppDispatch } from "../../redux/store";
 import { useAlert } from "../../hook/useAlert";
 import { LoginRequestBody } from "../../models/auth";
-
 
 const Login: React.FC = () => {
   const { showAlert, renderAlert } = useAlert();
@@ -14,7 +14,7 @@ const Login: React.FC = () => {
     password: "",
   });
 
-  const { name,email, password } = formData;
+  const { name, email, password } = formData;
 
   const dispatch = useAppDispatch();
 
@@ -27,22 +27,42 @@ const Login: React.FC = () => {
     setFormData(newForm);
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="section-log">
       <div className="log-box">
-        <form className="form" >
+        <form className="form" onSubmit={(e) => handleSubmit(e)}>
           <h2 className="heading-secondary">Log in</h2>
           <div className="form-control">
             <label>Email</label>
-            <input placeholder="Email" />
+            <input
+              value={email}
+              placeholder="Email"
+              type="email"
+              name="email"
+              onChange={(e) => handelChange(e)}
+              required
+            />
             <small>Please enter your Email</small>
           </div>
           <div className="form-control">
             <label>Password</label>
-            <input type="password" placeholder="Password" />
+            <input
+              value={password}
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={(e) => handelChange(e)} /*minLength="5"*/
+            />
             <small>Please enter your password</small>
           </div>
           <button>Submit</button>
+          <p className="my-1">
+            Don't have an account? <Link to="/register">Sign Up</Link>
+          </p>
         </form>
       </div>
     </div>
