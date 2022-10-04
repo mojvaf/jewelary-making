7b49cd17ff0ad4b518cbd9e86a7835a733d1edb3
mojvaf <mvafa44@gmail.com>
2022-10-04 14:40:27 +0200

    profile added

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -1,10 +1,44 @@
 import React from "react";
+import "./profile.css";
+import {
+  FaArrowAltCircleDown,
+  FaArrowAltCircleUp
+} from "react-icons/fa";
 
 
 const Profile = () => {
   return (
-    <section>
-      <p>Profile</p>
+    <section className="profile-container">
+      <div className="profile-header">
+        In this part we are going to add image of the user and the name
+      </div>
+      <article>This part is about news</article>
+      <aside>
+        <div className="profile-item">
+          <p className="profile-number">01</p>
+          <p className="profile-text">Do you want to invite friends?</p>
+          <FaArrowAltCircleUp className="profile-icon"/> 
+          <div className="profile-hidden-box">
+             <p>let us know</p>
+             <ul>
+              <li>lerem</li>
+              <li>lerem</li>
+             </ul>
+          </div>
+        </div>
+        <div className="profile-item">
+          <p className="profile-number">02</p>
+          <p className="profile-text">What is ?</p>
+          <FaArrowAltCircleDown className="profile-icon"/> 
+          <div className="profile-hidden-box">
+             <p>let us know</p>
+             <ul>
+              <li>lerem</li>
+              <li>lerem</li>
+             </ul>
+          </div>
+        </div>
+      </aside>
     </section>
   );
 };
