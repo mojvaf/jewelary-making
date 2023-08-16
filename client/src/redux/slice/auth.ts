60825475cbb73a3320f9646e265406766208d01d
mojvaf <mvafa44@gmail.com>
2023-08-16 11:34:04 +0200

    some changes

--- client/src/redux/slice/auth.ts
+++ client/src/redux/slice/auth.ts
@@ -1,28 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {setAlert} from './alert'
+
 type stateType = {
   token: string;
   isAuthenticated: boolean;
 };
-
+/** */
 const slice = createSlice({
   name: "auth",
   initialState: {
     token: localStorage.getItem("token"),
-    isAuthenticated: !!localStorage.getItem('token'),
+    isAuthenticated: !!localStorage.getItem("token"),
   } as stateType,
   reducers: {
-    login: (state: stateType, action: PayloadAction<{token:string}>) => {
-       state.token = action.payload.token 
-     localStorage.setItem('token', action.payload.token)
-     state.isAuthenticated= true
+    login: (state: stateType, action: PayloadAction<{ token: string }>) => {
+      state.token = action.payload.token;
+      localStorage.setItem("token", action.payload.token);
+      state.isAuthenticated = true;
     },
-    logout:(state:stateType)=>{
-      state.token= ''
-      localStorage.removeItem('token')
-      state.isAuthenticated=false
-    }
-  }
+    logout: (state: stateType) => {
+      state.token = "";
+      localStorage.removeItem("token");
+      state.isAuthenticated = false;
+    },
+  },
 });
 
 export const { login, logout } = slice.actions;
